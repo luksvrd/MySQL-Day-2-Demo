eb074cb5a4495cd6208972640c14f51895a8343e
luksvrd <ljvy9b@gmail.com>
2023-01-03 21:32:39 -0600

    not working

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ export default {
   db: {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    dbNAME: process.env.DB_NAME,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST || "localhost",
   },
 };
--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
-// Importing config
-import config from "./config.js";
+// Importing config with dotenv to keep out stuff private
 import mysql from "mysql2/promise";
+import config from "./config.js";
+import Connection from "mysql2/typings/mysql/lib/Connection.js";
 
-// This will show
-const db = mysql.createConnection(config.db);
+// This will be executed when the app starts
+const db = await mysql.createConnection(config.db);
+
+const [employees] = await conn.execute("SELECT * FROM employees LIMIT 10");
+console.log(results);
+conn.end();
